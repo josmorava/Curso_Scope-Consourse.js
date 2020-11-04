660c1fb7926bba9f0513036d74a0d0e1b9c3566e
josmorava <josemorales-1234@hotmail.es>
2020-11-04 02:38:10 -0400

    Functio Scope

---
+++ src/scope/function.js
@@ -0,0 +1,19 @@
+const fruits = () => {
+  var fruit = 'apple';
+  console.log(fruit);
+}
+
+fruits();
+
+const anotheFunction = () => {
+  var x = 1;
+  var x = 2;
+  let y = 1;
+  let y = 2;
+  console.log(x);
+  console.log(y);
+}
+
+anotheFunction();
+
+//Las variables let o const no pueden ser reasignar valores 
